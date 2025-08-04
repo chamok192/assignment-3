705cbdb945eb5fdb0dba227d5e6a6c28b92c5803
Chamok Bhadra <chamokbhadra@gmail.com>
2025-08-04 17:43:02 +0600

    problem6 solved

---
+++ problem6.js
@@ -0,0 +1,14 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 40;
+var startingSalary = 30000;
+//write your code here
+
+if (experience <= 50) {
+    var presentSalary=startingSalary;
+    for (var i = 1; i <= experience; i++) {
+        var increment = presentSalary * 0.05; 
+        presentSalary= presentSalary + increment;
+    }
+    console.log(presentSalary.toFixed(2));
+}
+
